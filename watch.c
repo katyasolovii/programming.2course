6e687b6fedfafd42748b913cbdaace76d35b5097
katyasolovii <144212333+katyasolovii@users.noreply.github.com>
2024-09-11 17:59:00 +0300

    Update watch.c

--- watch.c
+++ watch.c
@@ -1,27 +1,23 @@
 #include <stdio.h>
 #include <math.h>
 
+
 int main() {
     
-    double radian_hour, radian_minute;
+    double radian;
     int hours, minute;
 
-    printf("Enter radian of hour: ");
-    scanf("%lf", &radian_hour);
+    printf("Enter radian: ");
+    scanf("%lf", &radian);
 
-    printf("Enter radian of minute: ");
-    scanf("%lf", &radian_minute);
+    double degree = (radian * 180 / M_PI);
+    printf("%lf \n", degree);
 
+    // 1 година = 30 градусів
+    hours = (int)(degree / 30) % 12; 
+    // віднімаємо від загального години і знаходимо хвилин (60 хв / 30 градусів = 2 градуса)
+    minute = (int)((degree - (hours * 30)) * 2);
 
-    // Кожна година це 30 градусів (90 / 3 = 30, всього 12 годин)
-    // Кожна хвилина це 6 градуса (5 поділок між годинами, тому 30 / 5 = 6 градуса, всього 60 хвилин)
-
-    double degree_hour = (radian_hour * 180 / M_PI);
-    double degree_minute = (radian_minute * 180 / M_PI);
-
-    hours = (int)(degree_hour / 30) % 12; 
-    minute = (int)(degree_minute / 6) % 60; 
-
-    printf("Time is: %02d:%02d \n", hours, minute); 
+    printf("Time is: %02d:%02d \n", hours, minute);
 
 }
